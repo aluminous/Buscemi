d3f317df24d457cd9304295fbbe1db5f21966cfe
Austin Brown <austin@alumino.us>
2012-02-27 20:15:40 -0700

    Initial commit, v2

---
+++ src/com/akicode/busceni/BusceniActivity.java
@@ -0,0 +1,16 @@
+package com.akicode.busceni;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BusceniActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        
+        
+    }
+}
